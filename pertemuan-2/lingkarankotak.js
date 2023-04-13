bfec0df3e7dba33f67aa265f2f4f4bffee6e7f78
lamlam <hoilamkwan96@gmail.com>
2023-04-13 20:27:06 +0700

    tugas_21104410040_Kwan Hoi Lam

---
+++ pertemuan-2/lingkarankotak.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  background(240, 252, 3);
+  fill(252, 3, 132);
+  ellipse(150, 200, 250, 250);
+  
+  fill(24, 3, 252);
+  rect(350, 300, 250, 100);
+}
